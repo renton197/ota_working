cf1f4bb17ee50e715f44e22d23d95d9a7bd8f81a
Renton Ma <rma@ambiqmicro.com>
2016-12-13 10:55:53 +0800

    updated ota_binary_converter.py, replaced wrong file.

--- boards/apollo_evk_btle/examples/exactle_fit_amota/keil/ota_binary_converter.py
+++ boards/apollo_evk_btle/examples/exactle_fit_amota/keil/ota_binary_converter.py
@@ -27,126 +27,104 @@ def crc32(L):
 # Read in the binary files and output the merged binary
 #
 #******************************************************************************
-def process(boot_loader_filename, app_filename,  output):
-
-    # Open the file, and read it into an array of integers.
-    with open(boot_loader_filename, mode = 'rb') as f_bl:
-        boot_loader_binarray = f_bl.read()
-        f_bl.close()
+#def process(boot_loader_filename, app_filename,  output):
+def process(load_address, app_filename, app_ver, bin_type, output):
 
     # Open the file, and read it into an array of integers.
     with open(app_filename, mode = 'rb') as f_app:
         app_binarray = f_app.read()
         f_app.close()
 
-    boot_length = len(boot_loader_binarray)
-    print("boot size ",boot_length)
-    if boot_length > 16383:
-	    print("ERROR boot loader image is too big");
-	    return
-
     app_length  = len(app_binarray)
 
-    pad_length  = 0x4000 - len(boot_loader_binarray)
-
-    # this is where we will write the application size in FLASH
-    flag_page_location = 0x3c00 - len(boot_loader_binarray)
-
+    pad_length  = 48;   #fixed header length
     print("pad_length ",pad_length);
 
     #generate mutable byte array for the boot loader
     pad_binarray = bytearray([0]*pad_length);
 
+    # Insert encryption flag, always 0
+    pad_binarray[0]  = 0x00;
+    pad_binarray[1]  = 0x00;
+    pad_binarray[2]  = 0x00;
+    pad_binarray[3]  = 0x00;
+    
     # Insert the application binary load address.
-    #print("load_address ",hex(load_address), "(",load_address,")")
-    pad_binarray[flag_page_location + 0]  = 0x00;
-    pad_binarray[flag_page_location + 1]  = 0x40;
-    pad_binarray[flag_page_location + 2]  = 0x00;
-    pad_binarray[flag_page_location + 3]  = 0x00;
+    loadaddress = int(load_address,16)
+    print("load_address ",hex(loadaddress), "(",load_address,")")
+    pad_binarray[4]  = (loadaddress >>  0) & 0x000000ff;
+    pad_binarray[5]  = (loadaddress >>  8) & 0x000000ff;
+    pad_binarray[6]  = (loadaddress >> 16) & 0x000000ff;
+    pad_binarray[7]  = (loadaddress >> 24) & 0x000000ff;
+
 
     # put the application binary size into the padding array @ 0x3c04
     app_length  = len(app_binarray)
     print("app_size ",hex(app_length), "(",app_length,")")
-    pad_binarray[flag_page_location + 4]  = (app_length >>  0) & 0x000000ff
-    pad_binarray[flag_page_location + 5]  = (app_length >>  8) & 0x000000ff
-    pad_binarray[flag_page_location + 6]  = (app_length >> 16) & 0x000000ff
-    pad_binarray[flag_page_location + 7]  = (app_length >> 24) & 0x000000ff
+    pad_binarray[8]   = (app_length >>  0) & 0x000000ff
+    pad_binarray[9]   = (app_length >>  8) & 0x000000ff
+    pad_binarray[10]  = (app_length >> 16) & 0x000000ff
+    pad_binarray[11]  = (app_length >> 24) & 0x000000ff
 
     # compute the CRC for the application and write it to 0x3c08
     app_crc = crc32(app_binarray)
     print("crc =  ",hex(app_crc));
-    pad_binarray[flag_page_location + 8]  = (app_crc >>  0) & 0x000000ff
-    pad_binarray[flag_page_location + 9]  = (app_crc >>  8) & 0x000000ff
-    pad_binarray[flag_page_location + 10] = (app_crc >> 16) & 0x000000ff
-    pad_binarray[flag_page_location + 11] = (app_crc >> 24) & 0x000000ff
+    pad_binarray[12]  = (app_crc >>  0) & 0x000000ff
+    pad_binarray[13]  = (app_crc >>  8) & 0x000000ff
+    pad_binarray[14]  = (app_crc >> 16) & 0x000000ff
+    pad_binarray[15]  = (app_crc >> 24) & 0x000000ff
 
-    # override gpio. default 0
-    pad_binarray[flag_page_location + 12] = 0xff
-    pad_binarray[flag_page_location + 13] = 0xff
-    pad_binarray[flag_page_location + 14] = 0xff
-    pad_binarray[flag_page_location + 15] = 0xff
+    # override gpio. default -1 (ignored)
+    pad_binarray[16] = 0xff
+    pad_binarray[17] = 0xff
+    pad_binarray[18] = 0xff
+    pad_binarray[19] = 0xff
 
     # override polarity. default 0
-    pad_binarray[flag_page_location + 16] = 0
+    pad_binarray[20] = 0
 
     # copy the reset vector stack pointer (SP) from the application binary to pad_binary
-    pad_binarray[flag_page_location + 20] = app_binarray[0];
-    pad_binarray[flag_page_location + 21] = app_binarray[1];
-    pad_binarray[flag_page_location + 22] = app_binarray[2];
-    pad_binarray[flag_page_location + 23] = app_binarray[3];
+    pad_binarray[24] = app_binarray[0];
+    pad_binarray[25] = app_binarray[1];
+    pad_binarray[26] = app_binarray[2];
+    pad_binarray[27] = app_binarray[3];
 
     # copy the reset vector program counter (PC) from the application binary to pad_binary
-    pad_binarray[flag_page_location + 24] = app_binarray[4];
-    pad_binarray[flag_page_location + 25] = app_binarray[5];
-    pad_binarray[flag_page_location + 26] = app_binarray[6];
-    pad_binarray[flag_page_location + 27] = app_binarray[7];
+    pad_binarray[28] = app_binarray[4];
+    pad_binarray[29] = app_binarray[5];
+    pad_binarray[30] = app_binarray[6];
+    pad_binarray[31] = app_binarray[7];
 	
-	# rma: boot options
-    pad_binarray[flag_page_location + 28] = 0xff
-    pad_binarray[flag_page_location + 29] = 0xff
-    pad_binarray[flag_page_location + 30] = 0xff
-    pad_binarray[flag_page_location + 31] = 0xff
+	# rma: application software version here
+    print("app_ver",int(app_ver,16), "(",app_ver,")")
+    appver = int(app_ver,16)
+    pad_binarray[32]  = (appver >>  0) & 0x000000ff;
+    pad_binarray[33]  = (appver >>  8) & 0x000000ff;
+    pad_binarray[34]  = (appver >> 16) & 0x000000ff;
+    pad_binarray[35]  = (appver >> 24) & 0x000000ff;
 	
-    # rma: software version of the current image
-    pad_binarray[flag_page_location + 32] = 0xff
-    pad_binarray[flag_page_location + 33] = 0xff
-    pad_binarray[flag_page_location + 34] = 0xff
-    pad_binarray[flag_page_location + 35] = 0xff
+	# rma: binary type here
+    print("bin_type",int(bin_type,16), "(",bin_type,")")
+    bintype = int(bin_type,16)
+    pad_binarray[36]  = (bintype >>  0) & 0x000000ff;
+    pad_binarray[37]  = (bintype >>  8) & 0x000000ff;
+    pad_binarray[38]  = (bintype >> 16) & 0x000000ff;
+    pad_binarray[39]  = (bintype >> 24) & 0x000000ff;
 	
-    # rma: software version of the current image
-    pad_binarray[flag_page_location + 36] = 0xff
-    pad_binarray[flag_page_location + 37] = 0xff
-    pad_binarray[flag_page_location + 38] = 0xff
-    pad_binarray[flag_page_location + 39] = 0xff
+    # rma: storage type here, fixed to 0
+    pad_binarray[40] = 0
+    pad_binarray[41] = 0
+    pad_binarray[42] = 0
+    pad_binarray[43] = 0
 	
-    # rma: storage address
-    pad_binarray[flag_page_location + 40] = 0xff
-    pad_binarray[flag_page_location + 41] = 0xff
-    pad_binarray[flag_page_location + 42] = 0xff
-    pad_binarray[flag_page_location + 43] = 0xff
+	# rma: word RFU
+    pad_binarray[44] = 0xff
+    pad_binarray[45] = 0xff
+    pad_binarray[46] = 0xff
+    pad_binarray[47] = 0xff
 	
-	# rma: length of new image in bytes
-    pad_binarray[flag_page_location + 44] = 0xff
-    pad_binarray[flag_page_location + 45] = 0xff
-    pad_binarray[flag_page_location + 46] = 0xff
-    pad_binarray[flag_page_location + 47] = 0xff
-	
-	# rma: length of new image already stored/received in bytes
-    pad_binarray[flag_page_location + 48] = 0xff
-    pad_binarray[flag_page_location + 49] = 0xff
-    pad_binarray[flag_page_location + 50] = 0xff
-    pad_binarray[flag_page_location + 51] = 0xff
-	
-	# rma: length of new image already stored/received in bytes
-    pad_binarray[flag_page_location + 52] = 0xff
-    pad_binarray[flag_page_location + 53] = 0xff
-    pad_binarray[flag_page_location + 54] = 0xff
-    pad_binarray[flag_page_location + 55] = 0xff
-
-
     # now output all three binary arrays in the proper order
     with open(output + '.bin', mode = 'wb') as out:
-        out.write(boot_loader_binarray)
         out.write(pad_binarray)
         out.write(app_binarray)
 
@@ -156,9 +134,20 @@ def parse_arguments():
 
     # parser.add_argument('--bootbin', dest='bootbin', default='../../../../apollo_evk_base/examples/multi_boot/keil/bin/multi_boot.bin',
                         # help='Bootloader binary file (multi_boot.bin)')
+                        
+    parser.add_argument('--load-address', dest='loadaddress', default='0x4000',
+                        help='Load address of the application.')
 
     parser.add_argument('--appbin', dest='appbin', default='bin/exactle_fit_amota.bin',
                         help='Application binary file (app.bin)')
+                        
+    #rma: add arg version        	
+    parser.add_argument('--version', dest='app_ver', default='0x0',
+                        help = 'Software version of the OTA image.')
+    
+    #rma: add arg binary type        	
+    parser.add_argument('--binary-type', dest='bin_type', default='0x0',
+                        help = 'Binary type to be tranferred OTA.')
 
     parser.add_argument('-o', dest = 'output', default = 'binary_array',
                         help = 'Output filename (without the extension)')
@@ -176,7 +165,7 @@ def main():
     # Read the arguments.
     args = parse_arguments()
 
-    process(args.bootbin, args.appbin, args.output)
+    process(args.loadaddress, args.appbin, args.app_ver, args.bin_type, args.output)
 
 if __name__ == '__main__':
     main()
